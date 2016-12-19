44c56ccf72e5e18c08cca6cac58eb3cd200780a6
chiemnv <chiemnv90@gmail.com>
2016-12-19 09:31:42 +0700

    ok

--- demo/application/views/categories/add.php
+++
@@ -1,9 +0,0 @@
-<form action="" method="post">
-  Title:<br>
-  <input type="text" name="title" value="">
-  <br>
-  Url:<br>
-  <input type="text" name="url" value="">
-  <br><br>
-  <input type="submit" name="submit" value="Submit">
-</form> 
--- demo/application/views/categories/edit.php
+++
@@ -1,9 +0,0 @@
-<form action="" method="post">
-  Title:<br>
-  <input type="text" name="title" value="<?php echo isset($categories['title'])?$categories['title']:''; ?>">
-  <br>
-  Url:<br>
-  <input type="text" name="url" value="<?php echo isset($categories['url'])?$categories['url']:''; ?>">
-  <br><br>
-  <input type="submit" name="submit" value="Submit">
-</form> 
